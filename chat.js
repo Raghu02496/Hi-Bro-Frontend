858b1df802c0e4f5fa883adbe00f1960fd25b908
R <raghuramap41@gmail.com>
2025-05-08 11:36:23 +0530

    send message fn added

--- chat.js
+++ chat.js
@@ -1,1 +1,16 @@
-console.log('Hello world')
+function sendMessage(){
+    let messageBox = document.getElementById('message-content');
+    messageBox.value = ''
+
+    fetch('http://localhost:3000/info',{
+        method : 'POST',
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({
+            name: 'Alice',
+            age: 30
+        })
+    }).then(val=> val.json())
+    .then((val)=> console.log(val,'Response recieved'))
+}
