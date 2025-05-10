8e394d45cd0596697dfddbf09291b3a2d5a53280
Raghu <raghuramap41@gmail.com>
2025-05-10 17:28:36 +0530

    get cat pic functionality added

--- chat.js
+++ chat.js
@@ -1,3 +1,4 @@
+document.getElementById('cat-btn').addEventListener('click',getCat);
 function sendMessage(){
     let messageBox = document.getElementById('message-content');
     messageBox.value = ''
@@ -13,4 +14,16 @@ function sendMessage(){
         })
     }).then(val=> val.json())
     .then((val)=> console.log(val,'Response recieved'))
+}
+
+function getCat(){
+    fetch('http://localhost:3000/getCat',{
+        method : 'POST'
+    }).then(val=> val.blob()).then((val)=>{
+        let imgUrl = URL.createObjectURL(val);
+        let cat = document.createElement('img')
+        cat.src = imgUrl
+        document.querySelector('.cat-container').append(cat);
+        document.getElementById('cat-btn').removeEventListener('click',getCat,false);
+    })
 }
